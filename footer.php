d1d06b90a62eb8b794e52633254ed490255224a9
borzakap <borzakap@gmail.com>
2022-10-21 13:21:12 +0300

    implement social buttons

--- footer.php
+++ footer.php
@@ -13,18 +13,40 @@
 
 	<footer id="colophon" class="site-footer">
             <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-xs-12">
+                        <div class="footer-logo">
+                            <?php the_custom_logo(); ?>
+                        </div>
+                        <ul class="social-buttons">
+                            <?php if($contact_facebook = get_theme_mod( 'contact_facebook' )) : ?>
+                            <li class="link">
+                                <a class="facebook" href="<?= $contact_facebook ?>" target="_blank"></a>
+                            </li>
+                            <?php endif; ?>
+                            <?php if($contact_instagram = get_theme_mod( 'contact_instagram' )) : ?>
+                            <li class="link">
+                                <a class="instagram" href="<?= $contact_instagram ?>" target="_blank"></a>
+                            </li>
+                            <?php endif; ?>
+                            <?php if($contact_youtube = get_theme_mod( 'contact_youtube' )) : ?>
+                            <li class="link">
+                                <a class="youtube" href="<?= $contact_youtube ?>" target="_blank"></a>
+                            </li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-xs-12"></div>
+                    <div class="col-md-3 col-xs-12"></div>
+                    <div class="col-md-3 col-xs-12"></div>
+                </div>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ylia' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ylia' ), 'WordPress' );
+				printf( esc_html__( 'Proudly powered by %s', 'ylia' ), 'ZenSadhu' );
 				?>
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ylia' ), 'ylia', '<a href="https://automattic.com/">Automattic</a>' );
-				?>
 		</div><!-- .site-info -->
             </div>
 	</footer><!-- #colophon -->
--- inc/customizer.php
+++ inc/customizer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * _ylia Theme Customizer
  *
@@ -10,29 +11,108 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
-function ylia_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+function ylia_customize_register($wp_customize) {
+    $wp_customize->get_setting('blogname')->transport = 'postMessage';
+    $wp_customize->get_setting('blogdescription')->transport = 'postMessage';
+    $wp_customize->get_setting('header_textcolor')->transport = 'postMessage';
 
-	if ( isset( $wp_customize->selective_refresh ) ) {
-		$wp_customize->selective_refresh->add_partial(
-			'blogname',
-			array(
-				'selector'        => '.site-title a',
-				'render_callback' => 'ylia_customize_partial_blogname',
-			)
-		);
-		$wp_customize->selective_refresh->add_partial(
-			'blogdescription',
-			array(
-				'selector'        => '.site-description',
-				'render_callback' => 'ylia_customize_partial_blogdescription',
-			)
-		);
-	}
+    if (isset($wp_customize->selective_refresh)) {
+        $wp_customize->selective_refresh->add_partial(
+                'blogname',
+                array(
+                    'selector' => '.site-title a',
+                    'render_callback' => 'ylia_customize_partial_blogname',
+                )
+        );
+        $wp_customize->selective_refresh->add_partial(
+                'blogdescription',
+                array(
+                    'selector' => '.site-description',
+                    'render_callback' => 'ylia_customize_partial_blogdescription',
+                )
+        );
+    }
+    
+    /**
+     * Contacts pannel options
+     */
+    $wp_customize->add_section(
+            'pannel_contacts',
+            array(
+                'title' => __('Pannel contacts', 'ylia'),
+                'priority' => 130,
+            )
+    );
+    
+    /**
+     * Facebook page link
+     */
+    $wp_customize->add_setting(
+            'contact_facebook',
+            array(
+                'default' => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'transport' => 'postMessage',
+            )
+    );
+
+    $wp_customize->add_control(
+            'contact_facebook',
+            array(
+                'label' => __('Facebook link', 'ylia'),
+                'section' => 'pannel_contacts',
+                'type' => 'text',
+                'description' => __('Insert your Facebook page link here', 'ylia'),
+            )
+    );
+
+    /**
+     * Instagram page link
+     */
+    $wp_customize->add_setting(
+            'contact_instagram',
+            array(
+                'default' => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'transport' => 'postMessage',
+            )
+    );
+
+    $wp_customize->add_control(
+            'contact_instagram',
+            array(
+                'label' => __('Instagram link', 'ylia'),
+                'section' => 'pannel_contacts',
+                'type' => 'text',
+                'description' => __('Insert your Instagram page link here', 'ylia'),
+            )
+    );
+    
+    /**
+     * Youtube page link
+     */
+    $wp_customize->add_setting(
+            'contact_youtube',
+            array(
+                'default' => '',
+                'sanitize_callback' => 'sanitize_text_field',
+                'transport' => 'postMessage',
+            )
+    );
+
+    $wp_customize->add_control(
+            'contact_youtube',
+            array(
+                'label' => __('Youtube link', 'ylia'),
+                'section' => 'pannel_contacts',
+                'type' => 'text',
+                'description' => __('Insert your Youtube page link here', 'ylia'),
+            )
+    );
+    
 }
-add_action( 'customize_register', 'ylia_customize_register' );
+
+add_action('customize_register', 'ylia_customize_register');
 
 /**
  * Render the site title for the selective refresh partial.
@@ -40,7 +120,7 @@ add_action( 'customize_register', 'ylia_customize_register' );
  * @return void
  */
 function ylia_customize_partial_blogname() {
-	bloginfo( 'name' );
+    bloginfo('name');
 }
 
 /**
@@ -49,13 +129,15 @@ function ylia_customize_partial_blogname() {
  * @return void
  */
 function ylia_customize_partial_blogdescription() {
-	bloginfo( 'description' );
+    bloginfo('description');
 }
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function ylia_customize_preview_js() {
-	wp_enqueue_script( '_ylia-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _YLIA_VERSION, true );
+    wp_enqueue_script('_ylia-customizer', get_template_directory_uri() . '/js/customizer.js', array('customize-preview'), _YLIA_VERSION, true);
 }
-add_action( 'customize_preview_init', 'ylia_customize_preview_js' );
+
+add_action('customize_preview_init', 'ylia_customize_preview_js');
+
